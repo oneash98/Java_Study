4830057f1e2cfe005e901e24d2803cbf95484934
ONEASH <oneash082498@gmail.com>
2022-06-21 16:14:17 +0900

    Create Collection Framework

--- Do_it!_Introduction_to_Java_Programming/Chapter12/src/generics/GenericPrinterTest.java
+++ Do_it!_Introduction_to_Java_Programming/Chapter12/src/generics/GenericPrinterTest.java
@@ -5,13 +5,11 @@ public class GenericPrinterTest {
         GenericPrinter<Powder> powderPrinter = new GenericPrinter<Powder>();
 
         powderPrinter.setMaterial(new Powder());
-        Powder powder = powderPrinter.getMaterial();
         System.out.println(powderPrinter);
 
         GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<Plastic>();
 
         plasticPrinter.setMaterial(new Plastic());
-        Plastic plastic = plasticPrinter.getMaterial();
         System.out.println(plasticPrinter);
     }
 }
