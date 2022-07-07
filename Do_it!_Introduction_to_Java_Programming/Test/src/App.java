914b435c9b41802a245d83cb16a033a2c732a472
ONEASH <oneash082498@gmail.com>
2022-07-07 19:48:09 +0900

    Create final project - Student Class

--- Do_it!_Introduction_to_Java_Programming/Test/src/App.java
+++ Do_it!_Introduction_to_Java_Programming/Test/src/App.java
@@ -1,14 +1,14 @@
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        int i = 10;
-        switch(i) {
-            case 1: case 2: case 3: case 9:
-                System.out.println('a');
-                break;
-            case 4: case 5:
-                System.out.println('b');
-                break;
-            default: System.out.println('c');
+    public static void main(String[] args) throws IOException {
+        int i;
+
+        try{
+            i = System.in.read();
+            System.out.println((char)i);
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
 }
