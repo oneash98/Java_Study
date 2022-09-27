cc52eef71d398e24363c8d91f40fd104b7a945c6
ONEASH <oneash0824@naver.com>
2022-09-27 23:21:17 +0900

    Enum, more for wrapper class

--- Do_it!_Introduction_to_Java_Programming/Test/src/App.java
+++ Do_it!_Introduction_to_Java_Programming/Test/src/App.java
@@ -1,16 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 
 public class App {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("숫자, 이름");
-        
-        int i = scanner.nextInt();
-        scanner.nextLine();
-        String name = scanner.nextLine();
+    public static void create() {
 
-        System.out.println(i);
-        System.out.println(name);
+        try(BufferedReader reader = new BufferedReader(new FileReader("src/test.txt"))) {
+            int id = Integer.parseInt(reader.readLine());
+
+            try(BufferedWriter writer = new BufferedWriter(new FileWriter("src/test.txt", false))) {
+                writer.write(Integer.toString(id + 1));
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
     }
 }
